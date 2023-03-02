bfb4e612e47a81c84daaf2c6c746042fad6584a1
Leonardo Lanzieri <leonardolanzieri@hotmail.com>
2023-03-02 13:44:00 -0300

    Corrección módulos
    Corrección módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,33 +6,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module';
 import { EditarAlumnoDialogComponent } from './alumnos/components/editar-alumno-dialog/editar-alumno-dialog.component';
-import { Error404Component } from './core/components/error404/error404.component';
 import { FormatoTituloDirective } from './directives/formato-titulo.directive';
-import { InicioComponent } from './core/components/inicio/inicio.component';
-import { ModuloPersonalizadoModule } from './modulo-personalizado.module';
-import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
-import { ToolbarComponent } from './core/components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
-    NavbarComponent,
     FormatoTituloDirective,
     EditarAlumnoDialogComponent,
-    AgregarAlumnoDialogComponent,
-    InicioComponent,
-    Error404Component
+    AgregarAlumnoDialogComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AlumnosModule,
     SharedModule,
-    AppRoutingModule,
-    CoreModule
+    CoreModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,27 @@
+import { AppRoutingModule } from '../app-routing.module';
+import { Error404Component } from './components/error404/error404.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        InicioComponent,
+        Error404Component,
+        ToolbarComponent,
+        NavbarComponent,
+    ],
     imports: [
-
+        SharedModule,
+        AppRoutingModule
+    ],
+    exports: [
+        InicioComponent,
+        Error404Component,
+        ToolbarComponent,
+        NavbarComponent
     ],
     providers: []
 })
